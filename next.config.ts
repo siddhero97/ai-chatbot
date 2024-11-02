fe514d451e3695117e94adc47b3a9e2b7a36cf07
Siddhartha Gupta <siddhero97@gmail.com>
2024-11-02 23:10:05 +0000

    cache cdn hypothesis

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,20 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes under _next/static
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable', // Cache for 1 year
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

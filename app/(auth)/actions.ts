0f6260cf6d98c67d6400f6a135524d9682cb9a71
Siddhartha Gupta <siddhero97@gmail.com>
2024-11-03 03:19:08 +0000

    upating code to test force reload with remove cache

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -61,7 +61,7 @@ export const register = async (
       password: formData.get("password"),
     });
 
-    console.log("inside  register function 20:09 pst");
+    console.log("inside  register function 20:17 pst");
     let [user] = await getUser(validatedData.email);
 
     if (user) {

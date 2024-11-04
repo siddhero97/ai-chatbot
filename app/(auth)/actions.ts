f5690e1ab170d5e22fdf291c4a9d65e5f61ef9df
Siddhartha Gupta <siddhero97@gmail.com>
2024-11-04 19:13:03 +0000

    small change

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -61,7 +61,7 @@ export const register = async (
       password: formData.get("password"),
     });
 
-    console.log("inside  register function 20:59 pst");
+    console.log("inside  register function Monday Nov 4 11:12 pst");
     let [user] = await getUser(validatedData.email);
 
     if (user) {
